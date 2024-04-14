5591be331f5222c209a431c20c06c53e41fd53be
Chan Min Khant <jiyonfire@gmail.com>
2024-04-14 16:54:05 +0630

    Fix date calculation issue in DateDropdown.jsx(Final production)

--- src/utils/DateDropdown.jsx
+++ src/utils/DateDropdown.jsx
@@ -16,20 +16,30 @@ const DateDropdown = ({ setChoseDate, choseDate }) => {
 
   useEffect(() => {
     const today = new Date();
+
+    // Checking if the current time is greater than 7:30 AM or 7:00 AM
+    if (
+      today.getHours() > 7 ||
+      (today.getHours() === 7 && today.getMinutes() > 30)
+    ) {
+      today.setDate(today.getDate() + 1); // Move to the next day
+    }
+    console.log(today.getHours(), today.getMinutes());
     const nextDates = [];
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 10; i++) {
       const nextDay = new Date(today);
       nextDay.setDate(today.getDate() + i);
+
+      // Check if the time is greater than 6:10, if yes, increment the date
+
       const formattedDate = nextDay.toLocaleDateString('en-GB', options);
       nextDates.push(formattedDate);
     }
     setDates(nextDates);
     const storedDate = sessionStorage.getItem('selectedDate');
     if (nextDates.includes(storedDate)) {
-      console.log('1first');
       setChoseDate(storedDate);
     } else {
-      console.log('1second');
       setChoseDate(nextDates[0]);
     }
   }, []);
